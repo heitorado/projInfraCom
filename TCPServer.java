a663e21db91f4fbdff83016187f189482236696d
Heitor Sammuel Carvalho Souza <hscs@ug2c43.windows.cin.ufpe.br>
2019-05-07 09:08:26 -0300

    Create sample TCP Client and Server

---
+++ TCPServer.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.util.Date;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class TCPServer {
+    public static void main(String[] args) {
+
+        int port = 8001;
+
+        try {
+            ServerSocket server = new ServerSocket(port);
+            System.out.println("Server listening on port " + port);
+            while(true){
+                Socket client = server.accept();
+                System.out.println("A client has connected: " + client.getInetAddress().getHostAddress());
+                ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
+                out.flush();
+                out.writeObject(new Date());
+                out.close();
+                client.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
